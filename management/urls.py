fb760aa2b608b2c38378b601294f65e54f144935
EugeneAlimov <zeka_ali@i.ua>
2018-02-03 13:37:07 +0200

    частитчно восстановил

--- management/urls.py
+++ management/urls.py
@@ -21,4 +21,5 @@ from . import views
 urlpatterns = [
     path('', views.management, name='management'),
     path('management/', views.goods_adding, name='goods_adding'),
+    path('management/', views.goods_adding, name='goods_adding'),
 ]
--- management/views.py
+++ management/views.py
@@ -20,6 +20,20 @@ def goods_adding(request):
     return JsonResponse(return_dict)
 
 
+def goods_adding(request):
+    return_dict = dict()
+    print(request.POST)
+    data = request.POST
+    name_of_goods = data.get('name_of_goods')
+    price = data.get('price')
+
+    dsg = Product(name_of_product=name_of_goods, price=price)
+
+    dsg.save()
+
+    return JsonResponse(return_dict)
+
+
 
 def management(request):
     print('Ololo xyi')
